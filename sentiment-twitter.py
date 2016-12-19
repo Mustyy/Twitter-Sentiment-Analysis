769af150e072c5c9c1a29499da6f41f32ec3bb15
Kai Stullich <kaistullich@rocketmail.com>
2016-12-18 18:10:52 -0800

    moved all API keys to seperate file, and added .gitignore

--- sentiment-twitter.py
+++ sentiment-twitter.py
@@ -1,22 +1,22 @@
 import tweepy
 from textblob import TextBlob
+import config
 
-consumer_key = 'uNDuNX1PiLZAk7Fhr4vHMudbm'
-consumer_secret = 'doBqCf7qtHGQ1TVkuA8QzsshyKRllBrx5PxuHtZsrzFgM0r3Yr'
-
-access_token = '784783562402852864-VaqewzA8V6wBPavkmsfcJtdkM9FWSFK'
-access_token_secret = 'BWP7wSN5RM5tl0c8NLRqimiZnnLndCuHQd1Abzgm2IY2p'
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
-user_input = input('Enter the topic you would like to analyze: ')
+try:
+	print ('\n**** Twitter Sentiment Analysis ****\n')
+	user_input = input('Enter the topic you would like to analyze: ')
 
-public_tweets = api.search(str(user_input))
+	public_tweets = api.search(str(user_input))
 
-for tweet in public_tweets:
-	print (tweet.text)
-	analysis = TextBlob(tweet.text)
-	print(analysis.sentiment,'\n')
+	for tweet in public_tweets:
+		print (tweet.text)
+		analysis = TextBlob(tweet.text)
+		print(analysis.sentiment,'\n')
+
+except Exception as e:
+	print ('\nThe following error has occured:',str(e))
